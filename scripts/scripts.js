a074c086addd371c046ff36362f236195a38d95e
LCF2326 <150272082+lcf2326@users.noreply.github.com>
2023-12-01 02:28:23 -0500

    Initial Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+/*
+Student Name: Kartik Rameshchandra Rana
+Student ID: C0915199
+*/
+
+//Hamburger menu function
+function toggleNav() {
+    var nav = document.querySelector('nav');
+    nav.style.display = nav.style.display === 'none' || nav.style.display === '' ? 'block' : 'none';
+}
